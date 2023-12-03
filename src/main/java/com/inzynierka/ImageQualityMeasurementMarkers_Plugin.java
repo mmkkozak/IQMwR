4acb2b8fbe796003ed56cbb32aca05f05631e65d
Gysiaq <marcelina.mzyk24@gmail.com>
2023-12-03 21:53:26 +0100

    add macro

--- src/main/java/com/inzynierka/ImageQualityMeasurementMarkers_Plugin.java
+++ src/main/java/com/inzynierka/ImageQualityMeasurementMarkers_Plugin.java
@@ -25,9 +25,10 @@ public class ImageQualityMeasurementMarkers_Plugin implements PlugIn {
         GenericDialog gendialog = new GenericDialog("Image Quality Measurement Markers");
 
         int refImageIndex = 0;
-        int testImageIndex = 1;
+        int testImageIndex = 0;
         boolean[] modeStates = new boolean[]{true, true, true, true, true, true};
         String[] modeOptions = new String[]{"SSIM", "SNR", "CNR", "PSNR", "RMSE", "MAE"};
+        String refImg = "", testImg = "";
         int macro = 0;
 
         //Create results table
@@ -35,54 +36,59 @@ public class ImageQualityMeasurementMarkers_Plugin implements PlugIn {
         resultTable.incrementCounter();
         resultTable.setPrecision(6);
 
-        //List of images
-//        Opener opener = new Opener();
-//        ImagePlus image1 = opener.openImage("C:/Users/Admin/Desktop/Inzynierka_maven/Inzynierka/src/main/resources/images/Image1.png");
-//        ImagePlus image2 = opener.openImage("C:/Users/Admin/Desktop/Inzynierka_maven/Inzynierka/src/main/resources/images/Image2.png");
 
-//        List<ImagePlus> imageList = Arrays.asList();
+//        int[] windowImageList = WindowManager.getIDList();
+//        List<ImagePlus> imageList = Arrays.asList(image1, image2);
 //        int imageNumber = imageList.size();
+
         int imageNumber = WindowManager.getImageCount();
         String[] imageNames = new String[imageNumber];
 
         //Get name of images
         for (int i = 0; i < imageNumber; i++) {
             imageNames[i] = WindowManager.getImage(i + 1).getTitle();
-//            imageNames[i] = imageList.get(i).getTitle();
+//            imageNames[i] = windowImageList.get(i).getTitle();
         }
+
+//ERROR SECTION
 
         if (imageNumber < 2) {
             IJ.error("ERROR: No open image! You need 2");
             return;
         }
+//        imageList.subList(1, imageNumber).stream().forEach(testImage -> {
+//            if (imageList.get(0).getWidth() != testImage.getWidth() || imageList.get(0).getHeight() != testImage.getHeight())
+//                IJ.error("ERROR: Added images are not in the same size!");
+//        });
+
 
 //MACRO
         String macro_ij = Macro.getOptions();
 
         if (macro_ij == null) {
             //Generic Dialog
-            gendialog.addChoice("Reference Image", imageNames, imageNames[refImageIndex]);
-
-//            for (int i = 1; i < imageNumber; i++) {
-            gendialog.addChoice("Test Image", imageNames, imageNames[testImageIndex]);
-//            }
+            gendialog.addChoice("Reference Image", imageNames, imageNames[0]);
+            gendialog.addChoice("Test Image", imageNames, imageNames[1]);
             gendialog.addMessage("Markers");
             gendialog.addCheckboxGroup(modeOptions.length / 4 + 1, 4, modeOptions, modeStates);
-
             gendialog.showDialog();
+
 
             if (gendialog.wasOKed()) {
 
-                ArrayList<ImagePlus> testImages = new ArrayList<ImagePlus>(imageNumber);
+//            ArrayList<ImagePlus> testImages = new ArrayList<ImagePlus>(imageNumber);
 
                 refImageIndex = gendialog.getNextChoiceIndex();
-//           testImageIndex = gendialog.getNextChoiceIndex();
+                refImg = imageNames[refImageIndex];
 
-                for (int i = 0; i < imageNumber - 1; i++) {
-                    int idx = gendialog.getNextChoiceIndex();
-                    testImages.add(WindowManager.getImage(idx + 1));
-//                testImages.add(imageList.get(idx));
-                }
+                testImageIndex = gendialog.getNextChoiceIndex();
+                testImg = imageNames[testImageIndex];
+
+//            for (int i = 0; i < imageNumber - 1; i++) {
+//                int idx = gendialog.getNextChoiceIndex();
+////                testImages.add( WindowManager.getImage(idx + 1));
+//                testImages.add(windowImageList.get(idx));
+//            }
                 for (int i = 0; i < modeStates.length; i++) {
                     modeStates[i] = gendialog.getNextBoolean();
                 }
@@ -94,22 +100,21 @@ public class ImageQualityMeasurementMarkers_Plugin implements PlugIn {
                         calculateModes = true;
                 if (calculateModes == false)
                     return;
-
             }
         } else {
-            if (Macro.getOptions() != null) {
-                imageNames[refImageIndex] = Macro.getValue(Macro.getOptions(), "reference", "IMGREF");
-                imageNames[testImageIndex] = Macro.getValue(Macro.getOptions(), "test", "IMAGETEST");
-            }
-            else {
+            if (macro_ij != null) {
+                refImg = Macro.getValue(macro_ij, "reference", "IMGREF");
+                testImg = Macro.getValue(macro_ij, "test", "IMAGETEST");
+            } else {
                 IJ.error("ERROR: No arguments!");
                 return;
             }
         }
+
 //            ImagePlus refImage = imageList.get(refImageIndex);
-        ImagePlus refImage = WindowManager.getImage(refImageIndex + 1);
+        ImagePlus refImage = WindowManager.getImage(refImg);
 //          ImagePlus testImage = imageList.get(testImageIndex);
-        ImagePlus testImage = WindowManager.getImage(testImageIndex + 1);
+        ImagePlus testImage = WindowManager.getImage(testImg);
 
         if (refImage.getWidth() != testImage.getWidth()) {
             if (refImage.getHeight() != testImage.getHeight()) {
@@ -117,7 +122,6 @@ public class ImageQualityMeasurementMarkers_Plugin implements PlugIn {
                 return;
             }
         }
-
 
 //            testImages.stream().forEach(testImage -> {
         resultTable.addValue("Reference Image", refImage.getTitle());
@@ -149,8 +153,10 @@ public class ImageQualityMeasurementMarkers_Plugin implements PlugIn {
 
 //                resultTable.addRow();
 //            });
-//            resultTable.deleteRow(imageNumber - 1);
-        resultTable.show("Results");
+        String name = "";
+        name = "Results for: reference= " + refImg + "  " + "test= " + testImg;
+        resultTable.deleteRow(imageNumber - 1);
+        resultTable.show(name);
     }
 
 
--- src/main/java/com/inzynierka/Main.java
+++ src/main/java/com/inzynierka/Main.java
@@ -1,9 +1,25 @@
 package com.inzynierka;
 
+import ij.ImagePlus;
+import ij.Macro;
+import ij.WindowManager;
+import ij.gui.ImageWindow;
+import ij.io.Opener;
+
 public class Main {
     public static void main(String[] args){
         ImageQualityMeasurementMarkers_Plugin plugin = new ImageQualityMeasurementMarkers_Plugin();
 
+
+        Opener opener = new Opener();
+        ImagePlus image1 = opener.openImage("C:/Users/Admin/Desktop/Inzynierka_maven/Inzynierka/src/main/resources/images/Image1.png");
+        ImagePlus image2 = opener.openImage("C:/Users/Admin/Desktop/Inzynierka_maven/Inzynierka/src/main/resources/images/Image2.png");
+        WindowManager.addWindow(new ImageWindow(image1));
+        WindowManager.addWindow(new ImageWindow(image2));
+
+
+
+        plugin.run("hello world");
         plugin.run("hello world");
 
     }
