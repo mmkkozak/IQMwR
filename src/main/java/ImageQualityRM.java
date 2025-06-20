ced3a4b7e44fd77cc779630f0472e21954ac4284
mmkkozak <kmichalina000@gmail.com>
2025-06-20 18:42:28 +0200

    dropdown list with proper items and image choice validation - clean

--- src/main/java/ImageQualityRM.java
+++ src/main/java/ImageQualityRM.java
@@ -2,14 +2,12 @@ import net.imagej.ImageJ;
 import net.imagej.Dataset;
 import net.imagej.display.DatasetView;
 import net.imagej.display.ImageDisplay;
-import org.scijava.Initializable;
 import org.scijava.command.Command;
 import org.scijava.command.DynamicCommand;
 import org.scijava.display.Display;
 import org.scijava.module.MutableModuleItem;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.io.location.FileLocation;
 import org.scijava.ui.DialogPrompt;
 import org.scijava.ui.UIService;
 
@@ -19,7 +17,7 @@ import java.util.*;
 
 import net.imagej.display.ImageDisplayService;
 
-@Plugin(type = Command.class, menuPath = "Plugins>Image Quality Measures with Reference")
+@Plugin(type = DynamicCommand.class, menuPath = "Plugins>Image Quality Measures with Reference")
 public class ImageQualityRM extends DynamicCommand{
 
     @Parameter
@@ -31,14 +29,14 @@ public class ImageQualityRM extends DynamicCommand{
     @Parameter
     private ImageDisplayService imageDisplayService;
 
-    @Parameter(label="Reference Image", choices={})
-    private String referenceImage;
+    @Parameter(label="Reference Image")
+    private String referenceImageName;
 
     @Parameter(label="Test Image")
-    private String testImage;
+    private String testImageName;
 
-//    @Parameter(label = "Zastosuj metrykę PSNR")
-//    private boolean usePSNR;
+    @Parameter(label = "Zastosuj metrykę PSNR")
+    private boolean usePSNR;
 //
 //    @Parameter(label = "Zastosuj metrykę SSIM")
 //    private boolean useSSIM;
@@ -46,61 +44,34 @@ public class ImageQualityRM extends DynamicCommand{
 //    @Parameter(label = "Zastosuj metrykę MSE")
 //    private boolean useMSE;
 
-    private boolean imagesOpenFlag = false;
-
     @Override
     public void initialize() {
 
-//        List<ImageDisplay> openImages = imageDisplayService.getImageDisplays();
-        HashMap<String, ImageDisplay> nameToDataset = new HashMap<>();
+        // handling duplicates on dropdown list coming from parameters referenceImageName, testImageName
+        HashMap<String, ImageDisplay> imagesNames = new HashMap<>();
         for (ImageDisplay d : imageDisplayService.getImageDisplays()) {
             String name = d.getName();
-            if (!nameToDataset.containsKey(name)) {
-                nameToDataset.put(name, d);
+            if (!imagesNames.containsKey(name)) {
+                imagesNames.put(name, d);
             }
         }
-        List<String> names = new ArrayList<>(nameToDataset.keySet());
-
-        MutableModuleItem<String> refItem = getInfo().getMutableInput("referenceImage", String.class);
+        List<String> names = new ArrayList<>(imagesNames.keySet());
+        // assigning unique images names values to dropdown list
+        MutableModuleItem<String> refItem = getInfo().getMutableInput("referenceImageName", String.class);
         refItem.setChoices(names);
-
-        MutableModuleItem<String> testItem = getInfo().getMutableInput("testImage", String.class);
+        MutableModuleItem<String> testItem = getInfo().getMutableInput("testImageName", String.class);
         testItem.setChoices(names);
 
-//        if (imageDisplayService.getImageDisplays().size() < 2) {
-//            uiService.showDialog("Musisz mieć otwarte przynajmniej 2 obrazy.",
-//                    DialogPrompt.MessageType.ERROR_MESSAGE);
-//            return;
-//        } else {
-////            System.out.println(openImages);
-//            imagesOpenFlag = true;
-//        }
     }
 
     @Override
     public void run() {
 
-        if (!imagesOpenFlag) return;
-
-//        while (getOpenDatasetCount() < 2) {
-//            File file = uiService.chooseFile(null, "open");
-//            if (file == null) {
-//                uiService.showDialog("You need to open at least two images.", DialogPrompt.MessageType.ERROR_MESSAGE);
-//                return;
-//            }
-//
-//            Dataset dataset = null;
-//            try {
-//                dataset = ij.scifio().datasetIO().open(new FileLocation(file).getURI().toString());
-//            } catch (IOException e) {
-//                uiService.showDialog("Failed to open file:\n" + e.getMessage(), DialogPrompt.MessageType.ERROR_MESSAGE);
-//                return;
-//            }
-//            uiService.show(dataset);
-//        }
-
-//        System.out.println("Reference image: " + referenceImage.getName());
-//        System.out.println("Test image: " + testImage.getName());
+        if (referenceImageName.equals(testImageName)) {
+            uiService.showDialog("The same image was chosen as reference and test.", DialogPrompt.MessageType.WARNING_MESSAGE);
+        }
+        System.out.println("Reference image: " + referenceImageName);
+        System.out.println("Test image: " + testImageName);
 
 //        List<String> selectedMetrics = new ArrayList<>();
 //        if (usePSNR) selectedMetrics.add("PSNR");
@@ -109,9 +80,4 @@ public class ImageQualityRM extends DynamicCommand{
 
 //        System.out.println("Wybrane metryki: " + selectedMetrics);
     }
-
-//    protected int getOpenDatasetCount () {
-//        List<?> displays = imageDisplayService.getImageDisplays();
-//        return displays.size();
-//    }
 }
--- src/main/java/Preprocessor.java
+++ src/main/java/Preprocessor.java
@@ -1,73 +1,23 @@
-import net.imagej.Dataset;
-import net.imagej.DatasetService;
-import net.imagej.display.ImageDisplay;
 import net.imagej.display.ImageDisplayService;
-import org.scijava.command.CommandModuleItem;
 import org.scijava.module.Module;
-import org.scijava.module.ModuleInfo;
-import org.scijava.module.ModuleItem;
-import org.scijava.module.MutableModuleItem;
 import org.scijava.module.process.AbstractPreprocessorPlugin;
 import org.scijava.module.process.PreprocessorPlugin;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.ui.DialogPrompt;
-import org.scijava.ui.UIService;
-
-import java.util.*;
 
 @Plugin(type = PreprocessorPlugin.class)
 public class Preprocessor extends AbstractPreprocessorPlugin {
+
     @Parameter
     private ImageDisplayService displayService;
-    @Parameter
-    private UIService uiService;
 
     @Override
     public void process(Module module) {
         if (!module.getDelegateObject().getClass().equals(ImageQualityRM.class)) return;
 
-        HashMap<String, ImageDisplay> nameToDataset = new HashMap<>();
-        for (ImageDisplay d : displayService.getImageDisplays()) {
-            String name = d.getName();
-            if (!nameToDataset.containsKey(name)) {
-                nameToDataset.put(name, d);
-            }
+        // ensuring images to run the plugin are open
+        if (displayService.getImageDisplays().size() < 2) {
+            cancel("You need to open at least 2 images first.");
         }
-//        System.out.println("Name: " + nameToDataset.keySet());
-        if (nameToDataset.size() < 2) {
-//            uiService.showDialog("Otwórz co najmniej 2 obrazy.", DialogPrompt.MessageType.ERROR_MESSAGE);
-            cancel("You need to open at least 2 images.");
-            return;
-        }
-        List<String> names = new ArrayList<>(nameToDataset.keySet());
-//        String[] names =  new String[nameToDataset.size()];
-//        int i = 0;
-//        for (String name : nameToDataset.keySet()) {
-//            names[i] =  name;
-//            i++;
-//        }
-//        System.out.println(names.toString());
-        ModuleInfo info = module.getInfo();
-//        @SuppressWarnings("unchecked")
-//        MutableModuleItem<String> refItem = (MutableModuleItem<String>) info.getInput("referenceImage");
-//        refItem.setChoices(names);
-////        MutableModuleItem<String> test = module.getInfo().getInput("testImage");
-////        test.setChoices(names);
-//        @SuppressWarnings("unchecked")
-//        MutableModuleItem<String> testItem = (MutableModuleItem<String>) info.getInput("referenceImage");
-//        testItem.setChoices(names);
-//        for (String name : Arrays.asList("referenceImage", "testImage")) {
-//            ModuleItem<?> item = info.getInput(name);
-////            System.out.println(item.getChoices());
-//            if (item instanceof CommandModuleItem) {
-//                @SuppressWarnings("unchecked")
-//                CommandModuleItem<Dataset> cmdItem = (CommandModuleItem<Dataset>) item;
-//                cmdItem.set("choices", "names");
-//                System.out.println(cmdItem.is("choices"));
-//            }
-//        }
-
-        System.out.println("JEST");
     }
 }
